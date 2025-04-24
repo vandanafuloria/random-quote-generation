67078558b0b1ca089ec42e8eac4ab1c5fcf302cc
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-24 13:51:54 +0530

    ui and functionality part is completed

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const quoteBtn = document.getElementById("new-quote");
+
+function addToDom(obj) {
+  quoteEl.textContent = obj.quote;
+  authorEl.textContent = obj.author;
+}
+
+function generateQuote() {
+  const qoute = fetch("https://qapi.vercel.app/api/random")
+    .then((res) => {
+      console.log("wainting");
+      // res contain a stream of object
+      return res.json(); // jsonfy it;
+    })
+    .then((response) => {
+      addToDom(response);
+    })
+    .catch((err) => {
+      console.log("can not load");
+    });
+}
+
+quoteBtn.addEventListener("click", generateQuote);
