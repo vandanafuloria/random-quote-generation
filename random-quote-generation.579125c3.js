3dbfae45c581699ef19895a15598622eca4cfc29
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-24 18:06:48 +0530

    Updates

---
+++ random-quote-generation.579125c3.js
@@ -0,0 +1,35 @@
+const quoteEl = document.getElementById("quote");
+const authorEl = document.getElementById("author");
+const quoteBtn = document.getElementById("new-quote");
+const spinnerEl = document.querySelector(".spinner");
+const mainEl = document.querySelector(".main-page");
+const qoutebox = document.querySelector(".quote-box");
+function addToDom(obj) {
+    quoteEl.textContent = obj.quote;
+    authorEl.textContent = obj.author;
+}
+function generateQuote() {
+    const qoute = fetch("https://qapi.vercel.app/api/random").then((res)=>{
+        mainEl.style.background = "#FFFFFF80";
+        qoutebox.style.display = "none";
+        spinnerEl.style.display = "block";
+        // res contain a stream of object
+        return res.json(); // jsonfy it;
+    }).then((response)=>{
+        addToDom(response);
+        mainEl.style.background = "";
+        qoutebox.style.display = "block";
+        spinnerEl.style.display = "none";
+    }).catch((err)=>{
+        mainEl.style.background = "#FFFFFF80";
+        qoutebox.style.display = "none";
+        spinnerEl.style.display = "block";
+        console.log("can not load");
+    });
+}
+/************************************************* */ quoteBtn.addEventListener("click", generateQuote);
+document.addEventListener("keydown", (e)=>{
+    if (e.key === "Enter") generateQuote();
+});
+
+//# sourceMappingURL=random-quote-generation.579125c3.js.map
