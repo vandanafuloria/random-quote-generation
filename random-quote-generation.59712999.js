81317d4963c6d045d7ab7100e481c8cdd3e2c193
vandanafuloria <vandanafuloria02@gmail.com>
2025-04-25 00:04:20 +0530

    Updates

--- random-quote-generation.59712999.js
+++ random-quote-generation.59712999.js
@@ -160,11 +160,11 @@
       });
     }
   }
-})({"9JJ5D":[function(require,module,exports,__globalThis) {
+})({"erQ3N":[function(require,module,exports,__globalThis) {
 var global = arguments[3];
 var HMR_HOST = null;
 var HMR_PORT = null;
-var HMR_SERVER_PORT = 1234;
+var HMR_SERVER_PORT = 51098;
 var HMR_SECURE = false;
 var HMR_ENV_HASH = "d6ea1d42532a7575";
 var HMR_USE_SSE = false;
@@ -668,6 +668,6 @@ function hmrAccept(bundle /*: ParcelRequire */ , id /*: string */ ) {
 
 },{}],"4M6V8":[function(require,module,exports,__globalThis) {
 
-},{}]},["9JJ5D","4M6V8"], "4M6V8", "parcelRequire6fba", {})
+},{}]},["erQ3N","4M6V8"], "4M6V8", "parcelRequire6fba", {})
 
 //# sourceMappingURL=random-quote-generation.59712999.js.map
